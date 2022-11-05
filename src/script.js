96bda2e6fdb9efe59aec07cc41e351223f0c09a2
Ohuoba Chika <ohuobac@gmail.com>
2022-11-05 22:00:01 +0100

    update script.js
    made date and time current and worked on the search button to receive input and displace on h1

--- src/script.js
+++ src/script.js
@@ -1,38 +1,71 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  san_francisco: {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+let now = new Date();
+let date = now.getDate();
+let hours = now.getHours();
+let mins = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let currentDay = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let currentMonth = months[now.getMonth()];
 
-let city = prompt("Enter a city");
+let dayOfWeek = document.querySelector("#week-day");
+dayOfWeek.innerHTML = `${currentDay}`;
+let dayInteger = document.querySelector("#day");
+dayInteger.innerHTML = `${date}`;
+let monthOfYear = document.querySelector("#month");
+monthOfYear.innerHTML = `${currentMonth}`;
+let hourOfDay = document.querySelector("#hour");
+hourOfDay.innerHTML = `${hours}`;
+let minOfHour = document.querySelector("#min");
+minOfHour.innerHTML = `${mins}`;
 
-let getCity = weather.hasOwnProperty(city);
-if (getCity) {
-  let temperature = Math.round(weather[city].temp);
-  let fahrenheit = (temperature * 9) / 5 + 32;
-  let humidity = weather[city].humidity;
-  alert(
-    `It is currently ${temperature}°C (${fahrenheit}°F) in ${city} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
-  );
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  if (searchInput.value) {
+    h1.innerHTML = `${searchInput.value}`;
+  } else {
+    h1.innerHTML = null;
+    alert("Please type a City");
+  }
 }
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function celcius(event) {
+  event.preventDefault();
+  let tempID = document.querySelector("#temp");
+  tempID.innerHTML = "19";
+}
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", celcius);
+
+function fahrenheit(event) {
+  event.preventDefault();
+  let tempID = document.querySelector("#temp");
+  tempID.innerHTML = "66";
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", fahrenheit);
